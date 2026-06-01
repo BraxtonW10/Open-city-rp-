a7c8ab024da5d819114bb328fecddf90168fdc55
BraxtonW10 <161777065+BraxtonW10@users.noreply.github.com>
2026-06-01 07:36:28 -0500

    Create vehicle.js

---
+++ js/js/vehicle.js
@@ -0,0 +1,26 @@
+const vehicles = [
+{
+name: "Hellcat",
+color: "#222222",
+x: 600,
+y: 300
+},
+{
+name: "Lamborghini",
+color: "yellow",
+x: 900,
+y: 400
+},
+{
+name: "Ferrari",
+color: "red",
+x: 1200,
+y: 500
+},
+{
+name: "McLaren",
+color: "orange",
+x: 1500,
+y: 700
+}
+];
